0a0e08692895da480be8024ab99d557c53dc6ec0
Ozzy-ZY <Zyadhimself1@gmail.com>
2025-06-04 16:09:25 +0300

    Change Result Bool Flag into an int Flag to improve usability

--- APPLICATION/DTOs/Result.cs
+++ APPLICATION/DTOs/Result.cs
@@ -2,7 +2,7 @@
 
 public class Result
 {
-    public bool Success { get; set; }
+    public int StatusCode { get; set; }
     public string Message { get; set; }
     public object Data { get; set; }
 }
