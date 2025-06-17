df2f4f2192f43cfcb861e52f06797d836340f49d
Ozzy-ZY <Zyadhimself1@gmail.com>
2025-06-17 23:59:12 +0300

    Add GetTasksRequestValidator to enforce request validation

---
+++ APPLICATION/Validator/GetTasksRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using APPLICATION.DTOs;
+using FluentValidation;
+
+namespace APPLICATION.Validator;
+
+public class GetTasksRequestValidator:AbstractValidator<GetTasksRequest>
+{
+    public GetTasksRequestValidator()
+    {
+        RuleFor(r => r.FilterKey)
+            .MaximumLength(20)
+            .WithMessage("Filter Key must be less than 20 characters");
+        RuleFor(r => r.SortKey).MaximumLength(20).WithMessage("Sort Key must be less than 20 characters");
+        RuleFor(r => r.SortOrder).MaximumLength(10).WithMessage("type asc or desc only");
+    }
+}
